9338b37ceb002faab5e4da1983ea60a38d219554
FrancoBrechbuhl <37877874+FrancoBrechbuhl@users.noreply.github.com>
2020-02-28 20:12:14 -0300

    personalizacion de notificaciones push

--- app/src/main/java/com/B3B/farmbros/firebase/ServiceFirebase.java
+++ app/src/main/java/com/B3B/farmbros/firebase/ServiceFirebase.java
@@ -3,12 +3,15 @@ package com.B3B.farmbros.firebase;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.util.Log;
 
+import com.B3B.farmbros.Home;
 import com.B3B.farmbros.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -40,11 +43,23 @@ public class ServiceFirebase extends FirebaseMessagingService {
         String title = remoteMessage.getNotification().getTitle();
         String body = remoteMessage.getNotification().getBody();
 
+        Intent destinoNotificacion = new Intent(this, Home.class);
+        if(body.equals("Se ha respondido una consulta que realizaste")) {
+            destinoNotificacion.putExtra("profesion", "productor");
+        }
+        else {
+            destinoNotificacion.putExtra("profesion", "ingeniero");
+        }
+        destinoNotificacion.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, destinoNotificacion, 0);
+
         NotificationManagerCompat manager = NotificationManagerCompat.from(this);
         Notification notification = new NotificationCompat.Builder(this, "push_notification")
                 .setContentText(body)
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_notificacion_firebase)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build();
         manager.notify(0, notification);
     }
